e55387781b2d397526b671c9a8354b9484baee89
A00297514 <156479019+A00297514@users.noreply.github.com>
2024-02-16 23:38:00 +0000

    Commit with Unit test cases

--- TodoListTest/UnitTest1.cs
+++ TodoListTest/UnitTest1.cs
@@ -1,10 +1,44 @@
 namespace TodoListTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TodoListModels;
 
 [TestClass]
 public class UnitTest1
 {
     [TestMethod]
-    public void TestMethod1()
+    public void SetProperties()
     {
+        
+        var item = new ToDoItem();
+
+        item.Id = 1;
+        item.Description = "Test ToDo";
+        item.CompletedDate = DateTime.Now;
+
+        Assert.AreEqual(1, item.Id);
+        Assert.AreEqual("Test ToDo", item.Description);
+        Assert.IsNotNull(item.CompletedDate);
+    }
+
+    [TestMethod]
+    public void IdDefaultValueZero()
+    {
+        var item = new ToDoItem();
+        Assert.AreEqual(0, item.Id);
+    }  
+
+    [TestMethod]
+    public void DescriptionDefaultValueNull()
+    {  
+        var item = new ToDoItem();
+        Assert.IsNull(item.Description);
+    } 
+
+    [TestMethod]
+    public void CompletedDateDefaultValueNull()
+    { 
+        var item = new ToDoItem();
+        Assert.IsNull(item.CompletedDate);
     }
 }
